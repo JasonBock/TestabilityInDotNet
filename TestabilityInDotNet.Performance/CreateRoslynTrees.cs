ba10f2d3d8ce3b5d8284f1e02b216b0c01b30389
Jason Bock <jason@jasonbock.net>
2020-07-20 21:59:38 -0500

    minor updates

--- TestabilityInDotNet.Performance/CreateRoslynTrees.cs
+++ TestabilityInDotNet.Performance/CreateRoslynTrees.cs
@@ -3,7 +3,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-namespace BenchmarkTreeGeneration
+namespace TestabilityInDotNet.Performance
 {
 	[MemoryDiagnoser]
 	public class CreateRoslynTrees
--- TestabilityInDotNet.Performance/Program.cs
+++ TestabilityInDotNet.Performance/Program.cs
@@ -1,5 +1,4 @@
 ﻿using BenchmarkDotNet.Running;
-using BenchmarkTreeGeneration;
 
 namespace TestabilityInDotNet.Performance
 {
