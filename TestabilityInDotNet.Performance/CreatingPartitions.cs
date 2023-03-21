de885ff1add4aefa0b7f14cb5e6f5d701a2eee2a
Jason Bock <jason.r.bock@outlook.com>
2023-03-21 08:31:48 -0700

    Making some necessary updates

--- TestabilityInDotNet.Performance/CreatingPartitions.cs
+++ TestabilityInDotNet.Performance/CreatingPartitions.cs
@@ -1,8 +1,7 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Jobs;
-using Spackle.Extensions;
-using System;
+using Spackle;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 
@@ -26,16 +25,16 @@ namespace TestabilityInDotNet.Performance
 		}
 #pragma warning disable CA1812
 
-		public static IEnumerable<(Range, int)> NumberOfDigits()
+		public static IEnumerable<(Range<int>, int)> NumberOfDigits()
 		{
-			yield return (0..100, 4);
-			yield return (77..90431, 6);
-			yield return (6..9845, 21);
+			yield return (new(0, 100), 4);
+			yield return (new(77, 90431), 6);
+			yield return (new(6, 9845), 21);
 		}
 
 		[Benchmark]
 		[ArgumentsSource(nameof(CreatingPartitions.NumberOfDigits))]
-		public ImmutableArray<Range> Create((Range range, int numberOfPartitions) values) =>
+		public ImmutableArray<Range<int>> Create((Range<int> range, int numberOfPartitions) values) =>
 			values.range.Partition(values.numberOfPartitions);
 	}
 #pragma warning restore CA1822
