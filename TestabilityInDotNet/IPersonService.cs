ef6d4039ccf93a5288c3aa5696120fc03dd75d30
Jason Bock <jason.r.bock@outlook.com>
2023-03-23 07:27:43 -0700

    Changed service definition

--- TestabilityInDotNet/IPersonService.cs
+++ TestabilityInDotNet/IPersonService.cs
@@ -4,8 +4,6 @@ namespace TestabilityInDotNet
 {
 	public interface IPersonService
 	{
-		// If the id is not Guid.Empty, return a user.
-		// Else, throw an exception.
 		Person Get(Guid id);
 	}
 }
